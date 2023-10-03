544b2a3aa3f454a430e1b664ae342609c193bb05
Huakun Shen <huakun.shen@huakunshen.com>
2023-10-03 02:40:27 -0400

    Improve animation

--- packages/ui/components/ImageSelection.vue
+++ packages/ui/components/ImageSelection.vue
@@ -6,17 +6,20 @@ type UrlItem = {
   idx: number;
   swipeLeft: boolean;
   swipeRight: boolean;
+  candidate: boolean;
 };
-const urlsItems: Ref<UrlItem[]> = ref<UrlItem[]>(
+const urlsItems = ref<UrlItem[]>(
   props.urls.map((url, idx) => ({
     url,
     display: false,
     idx,
     swipeLeft: false,
     swipeRight: false,
+    candidate: true,
   }))
 );
 urlsItems.value[0].display = true;
+urlsItems.value[0].candidate = false;
 const urlDisplayIdx = ref<number>(0);
 const urlsItemToDisplay = computed(() => {
   return urlsItems.value.filter((item: UrlItem) => item.display);
@@ -26,8 +29,10 @@ function proceed() {
   urlDisplayIdx.value++;
   urlsItems.value[urlDisplayIdx.value].display = true;
   setTimeout(() => {
+    urlsItems.value[urlDisplayIdx.value].candidate = false;
+  }, 50);
+  setTimeout(() => {
     urlsItems.value[cacheIdx].display = false;
-    console.log(urlsItems.value[urlDisplayIdx.value]);
   }, 3000);
 }
 
@@ -52,25 +57,21 @@ onBeforeUnmount(() => {
 <template>
   <ul
     class="image-stack flex justify-center items-center relative overflow-x-hidden"
-    v-auto-animate
   >
     <li
       v-for="urlItem in urlsItemToDisplay"
-      :class="`absolute transition-all duration-300 ease-in-out ${
-        urlItem.swipeRight ? ' ml-[100vw]' : ''
-      } ${urlItem.swipeLeft ? ' mr-[100vw]' : ''}`"
+      :class="`absolute transition-all duration-300 ease-in-out
+      
+      ${urlItem.swipeRight ? ' ml-[100vw]' : ''} ${
+        urlItem.swipeLeft ? ' mr-[100vw]' : ''
+      }`"
       :key="urlItem.idx"
     >
-      <NuxtImg v-if="urlItem.display" width="600" :src="urlItem.url" />
+      <NuxtImg
+        v-if="urlItem.display"
+        :src="urlItem.url"
+        :class="`${urlItem.candidate ? 'w-1' : 'w-[30em]'} transition-all duration-300`"
+      />
     </li>
   </ul>
 </template>
-<style scoped>
-.image-stack .swipe-right {
-  margin-left: "100vw" !important;
-}
-
-.image-stack .swipe-left {
-  margin-right: "100vw" !important;
-}
-</style>
