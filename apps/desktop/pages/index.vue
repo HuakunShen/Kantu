c6f7e37f3ffb248d1c8dd92a6ad041318b763cf3
Huakun Shen <huakun.shen@huakunshen.com>
2023-10-04 10:48:46 -0400

    Fix desktop package name

--- apps/desktop/pages/index.vue
+++ apps/desktop/pages/index.vue
@@ -2,10 +2,13 @@
 const defaultStore = useDefaultStore();
 </script>
 <template>
-  <p class="pl-10 py-1">
-    <strong>Current Folder Path: </strong><code>{{ defaultStore.folder }}</code>
-  </p>
-  <div class="px-3 h-32">
-    <FileDrop />
+  <div class="flex flex-col h-full p-5">
+    <div class="px-3 grow">
+      <FileDrop />
+    </div>
+    <p class="pl-10 py-1 flex-none">
+      <strong>Current Folder Path: </strong
+      ><code>{{ defaultStore.folder }}</code>
+    </p>
   </div>
 </template>
