721cc5091e797dbd8ca092f006909289c0eeb4ca
Huakun Shen <huakun.shen@huakunshen.com>
2023-10-04 03:14:28 -0400

    Fixed desktop ui styling

---
+++ apps/desktop2/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="flex flex-col justify-around h-full px-4">
+    <NuxtLink to="/">
+      <UIcon class="text-4xl" name="i-ion-home-sharp" />
+    </NuxtLink>
+
+    <NuxtLink to="/gallery">
+      <UIcon class="text-4xl" name="i-icon-park-outline-preview-open" />
+    </NuxtLink>
+
+    <NuxtLink to="/image-classification">
+      <UIcon class="text-4xl" name="i-icon-park-outline-full-selection" />
+    </NuxtLink>
+
+    <NuxtLink to="/slideshow">
+      <UIcon class="text-4xl" name="i-tabler-slideshow" />
+    </NuxtLink>
+
+    <NuxtLink to="/setting">
+      <UIcon class="text-4xl" name="i-uil-setting" />
+    </NuxtLink>
+  </ul>
+</template>
--- apps/desktop2/pages/index.vue
+++ apps/desktop2/pages/index.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+const defaultStore = useDefaultStore();
+</script>
 <template>
-  <UButton>Hello</UButton>
+  <p class="pl-10 py-1">
+    <strong>Current Folder Path: </strong><code>{{ defaultStore.folder }}</code>
+  </p>
+  <div class="px-3 h-32">
+    <FileDrop />
+  </div>
 </template>
