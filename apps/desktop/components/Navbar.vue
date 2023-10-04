beb15342aaf1001225b0992ee18c61f3337e9226
Huakun Shen <huakun.shen@huakunshen.com>
2023-10-04 00:27:55 -0400

    Update Desktop navbar, ugly

--- apps/desktop/components/Navbar.vue
+++ apps/desktop/components/Navbar.vue
@@ -1,28 +1,32 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
 <template>
   <ul class="flex flex-col justify-around h-full px-4">
-    <ULink to="/">
+    <UButton to="/">
       Home
+      <Icon name="material-symbols:home" />
       <!-- <UIcon class="text-4xl" name="i-ion-home-sharp" /> -->
-    </ULink>
+    </UButton>
 
-    <ULink to="/gallery">
+    <UButton to="/gallery">
       Gallery
       <!-- <UIcon class="text-4xl" name="i-icon-park-outline-preview-open" /> -->
-    </ULink>
+    </UButton>
 
-    <ULink to="/image-classification">
+    <UButton to="/image-classification">
       Classification
       <!-- <UIcon class="text-4xl" name="i-icon-park-outline-full-selection" /> -->
-    </ULink>
+    </UButton>
 
-    <ULink to="/slideshow">
+    <UButton to="/slideshow">
       Slideshow
       <!-- <UIcon class="text-4xl" name="i-tabler-slideshow" /> -->
-    </ULink>
+    </UButton>
 
-    <ULink to="/setting">
+    <UButton to="/setting">
       Setting
       <!-- <UIcon class="text-4xl" name="i-uil-setting" /> -->
-    </ULink>
+    </UButton>
   </ul>
 </template>
