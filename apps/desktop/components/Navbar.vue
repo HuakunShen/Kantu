c79cc5817f94ed9ca005e7170459276d6edc1955
Huakun Shen <huakun.shen@huakunshen.com>
2023-10-04 03:15:36 -0400

    Reset Desktop Icon

--- apps/desktop/components/Navbar.vue
+++ apps/desktop/components/Navbar.vue
@@ -1,23 +1,23 @@
 <template>
   <ul class="flex flex-col justify-around h-full px-4">
     <NuxtLink to="/">
-      <UIcon class="text-4xl" name="i-ion-home-sharp" />
+      <UIcon class="text-3xl" name="i-ion-home-sharp" />
     </NuxtLink>
 
     <NuxtLink to="/gallery">
-      <UIcon class="text-4xl" name="i-icon-park-outline-preview-open" />
+      <UIcon class="text-3xl" name="i-icon-park-outline-preview-open" />
     </NuxtLink>
 
     <NuxtLink to="/image-classification">
-      <UIcon class="text-4xl" name="i-icon-park-outline-full-selection" />
+      <UIcon class="text-3xl" name="i-icon-park-outline-full-selection" />
     </NuxtLink>
 
     <NuxtLink to="/slideshow">
-      <UIcon class="text-4xl" name="i-tabler-slideshow" />
+      <UIcon class="text-3xl" name="i-tabler-slideshow" />
     </NuxtLink>
 
     <NuxtLink to="/setting">
-      <UIcon class="text-4xl" name="i-uil-setting" />
+      <UIcon class="text-3xl" name="i-uil-setting" />
     </NuxtLink>
   </ul>
 </template>
